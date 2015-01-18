8bf9950c344588d06d36bcdcaa07f88c632d3346
Michael Norris <mike@michaelnorris.co.uk>
2015-01-18 00:03:43 +0000

    changes to routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,15 +11,25 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'PagesController@home');
+
+
+if ( env('IMIN')) {
+    Route::get('/', 'PagesController@home');
+} else {
+    Route::get('/', 'PagesController@welcome');
+}
+Route::get('services', 'PagesController@services');
+Route::get('policies', 'PagesController@policies');
+Route::get('vision', 'PagesController@vision');
+Route::get('about', 'PagesController@about');
+Route::get('news', 'PagesController@news');
+Route::get('contact', 'PagesController@contact');
 
 Route::get('home', 'HomeController@index');
 
-Route::get('test', function() {
-	dd('test');
-});
 
 Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
+    'auth'     => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
 ]);
