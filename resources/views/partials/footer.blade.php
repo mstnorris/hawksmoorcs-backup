48fdac3a70109576f5d646c22ebc5c155c3f2571
Michael Norris <mike@michaelnorris.co.uk>
2015-01-18 13:26:22 +0000

    change column styles for mobile devices

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-5">
+            <div class="col-md-5 col-sm-6">
                 <span class="text-muted">&copy; {{ date('Y') }} Hawksmoor Construction Services Limited</span>
             </div>
             {{--<div class="col-md-2">--}}
@@ -10,21 +10,21 @@
                 {{--<a href="#" class="text-muted">Privacy</a><br />--}}
                 {{--<a href="#" class="text-muted">Terms</a><br />--}}
             {{--</div>--}}
-            <div class="col-md-2">
+            <div class="col-md-2 col-sm-3">
                 <a href="/about" class="text-muted">About</a><br />
                 {{--<a href="mailto:info@hawksmoorcs.co.uk" class="text-muted">Contact</a>--}}
                 <a href="/contact" class="text-muted">Contact</a><br />
                 {{--<a href="/careers" class="text-muted">Careers</a><br />--}}
                 {{--<a href="#" class="text-muted">Settings</a><br />--}}
             </div>
-            <div class="col-md-2">
+            <div class="col-md-2 col-sm-3">
                 <a href="https://www.facebook.com/hawksmoorcs" class=text-muted">Facebook</a><br />
                 <a href="https://twitter.com/hawksmoorcs" class=text-muted">Twitter</a><br />
                 {{--<a href="#" class=text-muted">Google</a><br />--}}
                 {{--<a href="#" class=text-muted">LinkedIn</a><br />--}}
             </div>
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-3 col-xs-hidden">
                     <address>
                         <strong>
                             Hawksmoor Construction<br />
