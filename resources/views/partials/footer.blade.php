93559e9740b23996e65f42d127bb2548a2430597
Michael Norris <mike@michaelnorris.co.uk>
2015-01-18 13:59:40 +0000

    removed address from footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-5 col-sm-6">
+            <div class="col-md-6 col-sm-6 col-xs-12">
                 <span class="text-muted">&copy; {{ date('Y') }} Hawksmoor Construction Services Limited</span>
             </div>
             {{--<div class="col-md-2">--}}
@@ -10,39 +10,18 @@
                 {{--<a href="#" class="text-muted">Privacy</a><br />--}}
                 {{--<a href="#" class="text-muted">Terms</a><br />--}}
             {{--</div>--}}
-            <div class="col-md-2 col-sm-3">
+            <div class="col-md-3 col-sm-3 col-xs-6">
                 <a href="/about" class="text-muted">About</a><br />
                 {{--<a href="mailto:info@hawksmoorcs.co.uk" class="text-muted">Contact</a>--}}
                 <a href="/contact" class="text-muted">Contact</a><br />
                 {{--<a href="/careers" class="text-muted">Careers</a><br />--}}
                 {{--<a href="#" class="text-muted">Settings</a><br />--}}
             </div>
-            <div class="col-md-2 col-sm-3">
+            <div class="col-md-3 col-sm-3 col-xs-6">
                 <a href="https://www.facebook.com/hawksmoorcs" class=text-muted">Facebook</a><br />
                 <a href="https://twitter.com/hawksmoorcs" class=text-muted">Twitter</a><br />
                 {{--<a href="#" class=text-muted">Google</a><br />--}}
                 {{--<a href="#" class=text-muted">LinkedIn</a><br />--}}
-            </div>
-            <div class="row">
-                <div class="col-md-3 hidden-xs">
-                    <address>
-                        <strong>
-                            Hawksmoor Construction<br />
-                            Services Limited
-                        </strong>
-                        <br />
-                        <br />
-                        Merryfields<br>
-                        Church Street<br>
-                        Boxted<br>
-                        CO4 5SX<br>
-                        <br />
-                        <abbr title="Phone">P:</abbr>&nbsp;+44&nbsp;(0)&nbsp;1206&nbsp;271&nbsp;577<br>
-                        <abbr title="Fax">F:</abbr>&nbsp;+44&nbsp;(0)&nbsp;1206&nbsp;271&nbsp;577<br>
-                        <br />
-                        <abbr title="Email">E:</abbr>&nbsp;<a href="mailto:info@hawksmoorcs.co.uk">info@hawksmoorcs.co.uk</a>
-                    </address>
-                </div>
             </div>
         </div>
     </div>
